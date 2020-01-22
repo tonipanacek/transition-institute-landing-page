97c953a79cbd1db7d01fb8db4214064d5f6d57ba
Toni Panacek <toni.panacek@gmail.com>
2020-01-22 15:49:42 +0100

    initial commit and landing page text added

---
+++ gridsome.config.js
@@ -0,0 +1,38 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/styles/*.scss'),
+      ],
+    });
+}
+
+module.exports = {
+  siteName: 'Transition Institute',
+  siteUrl: 'https://www.transitioninstitute.org',
+  plugins: [
+  ],
+  chainWebpack (config) {
+    // Load style variables for all vue-files
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type));
+    });
+
+    // SVG
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  }
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <Layout class="main">
+    <h1>Transition Institute</h1>
+    <p>Coming soon</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Transition Institute'
+  }
+}
+</script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Libre+Caslon+Text:400,700&display=swap');
+  body {
+    font-family: 'Libre Caslon Text';
+  }
+  .main {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 0;
+    margin: 0 auto;
+  }
+</style>
